f6174138bf2b69f0152ffedc813070dc6b104db7
dinggul56 <dinggul56@gmail.com>
2024-12-12 21:38:42 +0900

    feat: input box 컴포넌트 추가가

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BoardItem from 'components/BoardItem';
 import { latestBoardListMock, top3BoardListMock, commentListMock, favoriteListMock } from 'mocks';
 import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/InputBox';
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
+
   return (
     <>
-      <div style={{display: "flex", columnGap: '30px', rowGap: '20px'}}>
-        {favoriteListMock.map(favoriteItem => <FavoriteItem favoriteListItem={favoriteItem}/>)}
-      </div>
+      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue}/>
     </>
   );
 }

05c0b160d0837be8a0f9869b0cfc814b14f59dc9
dinggul56 <dinggul56@gmail.com>
2024-12-12 11:28:13 +0900

    feat: 게시판 리스트 컴포넌트 추가

---
+++ board-front/src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,67 @@
+import { BoardListItem } from 'types/interface';
+
+const latestBoardListMock: BoardListItem[] = [
+    {
+        "boardNumber": 1,
+        "title": "취준생 포트폴리오 피드백 정중히 부탁드립니다",
+        "content": "안녕하세요. 26살 퍼블리셔 취준생입니다.  경력,,을 간단하게 말씀드리자면 국비 6개월 후 웹디자인/퍼블리싱으로 1년 6개월의 경력이 있구요, 개인 사정으로 퇴사를 하고 프론트엔드로 업무 전환을 하려고 부트캠프 수료를 했습니다.  포트폴리오는 국비 수료 당시 만들었던 포트폴리오고 부트캠프 수료 후, 업데이트를 시킨 버전입니다.  직무를 프론트엔드가 아닌 퍼블리셔로 초점을 맞춘 이유는 첫 번째, 미미하지만 경험해 본 업무고 두 번째, 퍼블리셔 공고를 보니 React까지 할 수 있는 퍼블리셔 구인 공고가 많더라구요.",
+        "boardTitleImage": 'https://cdn.pixabay.com/photo/2024/09/05/11/52/cats-9024710_1280.jpg',
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDateTime": "2024. 12. 11. 9:35:23",
+        "writerNickname": "ㄱㄱㄹ",
+        "writerProfileImage": 'https://cdn.pixabay.com/photo/2024/11/08/05/28/man-9182458_1280.jpg',
+    },
+    {
+        "boardNumber": 1,
+        "title": "인공지능 코드의 40% 는 단기간내 사람에 의해 수정된다...",
+        "content": "뉴스 기사중 하나였는데... 에러때문인가.. 하고 넘어갔는데 지금 보니까 성능상의 문제 또는 에러 안나는 논리적인 오류때문이네요. 리뷰가 더 중요해지는...",
+        "boardTitleImage": 'https://cdn.pixabay.com/photo/2023/06/04/23/09/desert-8041046_1280.jpg',
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDateTime": "2024. 12. 12. 10:15:00",
+        "writerNickname": "엘피타",
+        "writerProfileImage": 'https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg',
+    },
+    {
+        "boardNumber": 1,
+        "title": "인공지능 코드의 40% 는 단기간내 사람에 의해 수정된다...",
+        "content": "뉴스 기사중 하나였는데... 에러때문인가.. 하고 넘어갔는데 지금 보니까 성능상의 문제 또는 에러 안나는 논리적인 오류때문이네요. 리뷰가 더 중요해지는...",
+        "boardTitleImage": null,
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDateTime": "2024. 12. 12. 10:15:00",
+        "writerNickname": "엘피타",
+        "writerProfileImage": null,
+    },
+    {
+        "boardNumber": 1,
+        "title": "인공지능 코드의 40% 는 단기간내 사람에 의해 수정된다...",
+        "content": "뉴스 기사중 하나였는데... 에러때문인가.. 하고 넘어갔는데 지금 보니까 성능상의 문제 또는 에러 안나는 논리적인 오류때문이네요. 리뷰가 더 중요해지는...",
+        "boardTitleImage": 'https://cdn.pixabay.com/photo/2023/06/04/23/09/desert-8041046_1280.jpg',
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDateTime": "2024. 12. 12. 10:15:00",
+        "writerNickname": "엘피타",
+        "writerProfileImage": 'https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg',
+    },
+    {
+        "boardNumber": 1,
+        "title": "인공지능 코드의 40% 는 단기간내 사람에 의해 수정된다...",
+        "content": "뉴스 기사중 하나였는데... 에러때문인가.. 하고 넘어갔는데 지금 보니까 성능상의 문제 또는 에러 안나는 논리적인 오류때문이네요. 리뷰가 더 중요해지는...",
+        "boardTitleImage": 'https://cdn.pixabay.com/photo/2023/06/04/23/09/desert-8041046_1280.jpg',
+        "favoriteCount": 0,
+        "commentCount": 0,
+        "viewCount": 0,
+        "writeDateTime": "2024. 12. 12. 10:15:00",
+        "writerNickname": "엘피타",
+        "writerProfileImage": 'https://www.handmk.com/news/photo/202306/16714_40371_5250.jpg',
+    }
+
+];
+
+export default latestBoardListMock;

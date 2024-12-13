faaa5a6f03ea63151f4dbe3da6b44371a8438bb9
dinggul56 <dinggul56@gmail.com>
2024-12-13 21:36:57 +0900

    feat: footer 컴포넌트 초기화화

---
+++ board-front/src/layouts/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css';
+
+//          component: 푸터 레이아웃         //
+export default function Footer() {
+
+    //          render: 푸터 렌더링          //
+  return (
+    <div id='footer'>
+        <div className="footer-container"></div>
+
+    </div>
+  )
+}

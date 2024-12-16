758e5ca8e51b5d87f7638908427f35f1296a2c0f
dinggul56 <dinggul56@gmail.com>
2024-12-16 11:23:57 +0900

    feat: View 폴더 생성 후 index파일과 css 파일 생성성

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -7,14 +7,24 @@ import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
 import InputBox from 'components/InputBox';
 import Footer from 'layouts/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 
+//          component: Application 컴포넌트          //
 function App() {
 
+  //          render: Application 컴포넌트 렌더링          //
+  // description: 메인화면: '/' - Main //
+  // description: 로그인 + 회원가입 화면: '/auth' - Authentication //
+  // description: 검색 화면: '/search/:searchWord' - Search //
+  // description: 유저 페이지: '/user/:userEmail' - User //
+  // description: 게시물 상세보기: '/board/detail/:boardNumber' - BoardDetail //
+  // description: 게시물 작성하기: '/board/write' - BoardWrite //
+  // description: 게시물 수정하기: '/board/update/:boardNumber' - BoardUpdate //
   return (
-    <>
-      <Footer />
-    </>
+    <Routes>
+      {/* <Route path='/' element={<Main />} /> */}
+    </Routes>
   );
 }
 

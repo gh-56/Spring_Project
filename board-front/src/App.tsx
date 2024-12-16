bf7933fe0b9e3dd6cd4b76b50a0583195938289b
dinggul56 <dinggul56@gmail.com>
2024-12-16 11:08:03 +0900

    feat: Footer 컴포넌트 추가
    Inter 폰트 추가.
    로고 이미지 추가
    css 적용용

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -6,14 +6,14 @@ import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
 import InputBox from 'components/InputBox';
+import Footer from 'layouts/Footer';
+
 
 function App() {
 
-  const [value, setValue] = useState<string>('');
-
   return (
     <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue}/>
+      <Footer />
     </>
   );
 }
